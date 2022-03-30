30e5c7dd76599cae68cd9f5f84d97d36e02eb4cb
jacob96DD <jacob110396@outlook.com>
2022-03-30 13:23:05 +0200

    more form

--- app/routes/new.jsx
+++ app/routes/new.jsx
@@ -2,31 +2,60 @@ import { Link, redirect, useActionData, Form, json } from "remix";
 import Button from "~/components/Button.jsx";
 import connectDb from "~/db/connectDb.server.js";
 
+export const action = async ({ request }) => {
+    const formData = await request.formData();
+  
+    const language = formData.get("language");
+    const title = formData.get("title");
+    const code = formData.get("code");
+    const description = formData.get("description");
+  
+    const errors = {};
+    if (!title) errors.title = true;
+    if (!description) errors.description = true;
+  
+    if (Object.keys(errors).length) {
+      const values = Object.fromEntries(formData);
+      return json({ errors, values });
+    }
+  
+    await fetch(`http://localhost:3000/api/kids-fashion/`, {
+      method: "POST",
+      body: JSON.stringify({ title, description, id: uuid }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    // TODO: Make a POST request via fetch to an API route that receives JSON data
+    // and creates the product in the db
+    // throw new Error("POST handler not implemented");
+  
+    return redirect(`../`);
+  };
+
 
   export default function NewSnippet() {
     const actionData = useActionData();
     return (
 
-        <><Label htmlFor="title">Title</Label>
+   
             <Form method="post" className="postForm">
-            <input type="text" id="text" name="text"></input>
-            <input type="text" id="title" name="title"></input>
-            <input type="text" id="code" name="code"></input>
-            <input type="text" id="description" name="description"></input>
+                 <label htmlFor="language">Language</label>
+            <input type="text" id="language" name="language" defaultValue={actionData?.values.language}></input>
 
+                <label htmlFor="title">Title</label>
+            <input type="text" id="title" name="title" defaultValue={actionData?.values.title}></input>
 
-            {actionData?.errors.name ? (
-                <p style={{ color: "red" }}>{actionData.errors.name}</p>
-            ) : null}
+                <label htmlFor="code">Code</label>
+            <textarea type="text" id="code" name="code" className="bigField" defaultValue={actionData?.values.code}></textarea>
 
-            {actionData?.errors.description ? (
-                <p style={{ color: "red" }}>{actionData.errors.description}</p>
-            ) : null}
+                <label htmlFor="description">Description</label>
+            <textarea type="text" id="description" name="description" className="bigField" defaultValue={actionData?.values.description}></textarea>
 
             <div className="submitbutton">
                 <Button type="submit">Add snippet</Button>
             </div>
-        </Form></>
+        </Form>
       
 
     );
